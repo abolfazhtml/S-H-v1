e8c3b82af483282c73e0caf46b19a20bb9b05abf
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2024-12-04 13:51:56 +0330

    first update
    mmm

---
+++ Java-script/app.js
@@ -0,0 +1,90 @@
+const d = new Date();
+let year = d.getFullYear();
+let day = d.getDay();
+let month = d.getMonth();
+
+if (month == 0) {
+    month = "January";
+}
+if (month == 1) {
+    month = "February";
+}
+if (month == 2) {
+    month = "March";
+}
+if (month == 3) {
+    month = "April";
+}
+if (month == 4) {
+    month = "May";
+}
+if (month == 5) {
+    month = "Juan";
+}
+if (month == 6) {
+    month = "July";
+}
+if (month == 7) {
+    month = "August";
+}
+if (month == 8) {
+    month = "September";
+}
+if (month == 9) {
+    month = "October";
+}
+if (month == 10) {
+    month = "November";
+}
+if (month == 11) {
+    month = "December";
+}
+console.log(typeof day);
+
+if (day == 0) {
+    day = "Sunday";
+}
+if (day == 1) {
+    day = "Monday";
+}
+if (day == 2) {
+    day = "Tuesday";
+}
+if (day == 3) {
+    day = "Wednesday";
+}
+if (day == 4) {
+    day = "Thursday";
+}
+if (day == 5) {
+    day = "Friday";
+}
+if (day == 6) {
+    day = "Saturday";
+}
+
+// document.getElementById("year").innerHTML = year;
+// document.getElementById("month").innerHTML = month;
+// document.getElementById("day").innerHTML = day;
+//
+// let lista = document.getElementById("list-r-1").innerHTML;
+// let listb = document.getElementById("list-r-2").innerHTML;
+// let listc = document.getElementById("list-r-3").innerHTML;
+// let listd = document.getElementById("list-r-4").innerHTML;
+// let liste = document.getElementById("list-r-5").innerHTML;
+//
+// lista = "Wed hell 8:00AM — 6:00PM";
+// // listb = "Thu 8:00AM — 6:00PM";
+
+
+
+
+
+
+
+
+
+
+
+
+
